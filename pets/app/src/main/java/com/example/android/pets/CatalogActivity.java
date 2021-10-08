bf0d6298ef1d292f59e51f36e426ea9009aab867
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-08 17:21:56 +0530

    Connected Provider insert() Method

--- pets/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ pets/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -19,6 +19,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -128,17 +129,23 @@ public class CatalogActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
+     */
     private void insertPet() {
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
+        // Create a ContentValues object where column names are the keys,
+        // and Toto's pet attributes are the values.
         ContentValues values = new ContentValues();
-
         values.put(PetEntry.COLUMN_PET_NAME, "Toto");
         values.put(PetEntry.COLUMN_PET_BREED, "Terrier");
         values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDER_MALE);
-        values.put(PetEntry.COLUMN_PET_WEIGHT, 2);
+        values.put(PetEntry.COLUMN_PET_WEIGHT, 7);
 
-        db.insert(PetEntry.TABLE_NAME, null, values);
+        // Insert a new row for Toto into the provider using the ContentResolver.
+        // Use the {@link PetEntry#CONTENT_URI} to indicate that we want to insert
+        // into the pets database table.
+        // Receive the new content URI that will allow us to access Toto's data in the future.
+        Uri newUri = getContentResolver().insert(PetEntry.CONTENT_URI, values);
     }
 
     @Override
